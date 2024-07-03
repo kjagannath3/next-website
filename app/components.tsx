cfc3bc841b4511b6dfd14488ab3bf683abb928a6
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2024-07-02 22:36:12 -0400

    Redid basic component skeleton

--- app/components.tsx
+++ app/components.tsx
@@ -1,7 +1,42 @@
 import React from "react";
-export function NameIcon(){
+export function NameIcon() {
     return (
-        <div><h1>Hi, I'm</h1><h1>Keshav</h1>
+        <div>
+            <h1>Hi, I'm</h1>
+            <h1>Keshav</h1>
         </div>
-    )
+    );
+}
+
+export function BlurbName() {
+    return (
+        <div>
+            <h1>Insert blurb here</h1>
+            <h1>Insert Image here</h1>
+        </div>
+    );
+}
+
+export function ProjectsList() {
+    return (
+        <div>
+            <h1>Insert projects here</h1>
+        </div>
+    );
+}
+
+export function ResumeEmbed() {
+    return (
+        <div>
+            <h1>Insert resume here</h1>
+        </div>
+    );
+}
+
+export function CallToAction() {
+    return (
+        <div>
+            <h1> Insert call to action here </h1>
+        </div>
+    );
 }
