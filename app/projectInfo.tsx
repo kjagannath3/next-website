e3117faa6d83ba425bcbcc7f39811b36b53de919
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2024-07-06 23:41:50 -0400

    More work on projectsList and compartmentalisation

---
+++ app/projectInfo.tsx
@@ -0,0 +1,18 @@
+
+
+
+
+export function NeuralNet() {
+    return (
+   <> 
+            <h1>Music Neural Network</h1>
+            <h1>I created a neural network for music!</h1>{" "}
+            <ul>
+              {" Hello World"}
+              <li>Python</li> <li>Tensorflow</li> <li>Numpy</li>{" "}
+              <li>The Nsynth Dataset</li>
+            </ul>
+            </>
+
+    )
+}
